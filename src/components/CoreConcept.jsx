f96ed33b126729b862b96baf9d25126da68fb7e9
Dankbudbaker <Dankbudbaker@protonmail.com>
2024-10-01 19:02:22 -0500

    Update

--- src/components/CoreConcept.jsx
+++ src/components/CoreConcept.jsx
@@ -10,15 +10,32 @@ export default function CoreConcept(props) {
       <h3>{props.title}</h3>
       <p>{props.description}</p>
       <div id="tab-content-two">
-        <span>Purchase</span>
+        {(props.escrowLink || props.opensourceLink) && <span>Purchase</span>}
+        
+        {props.escrowLink && (
+          <button className="active" onClick={() => navigateTo(props.escrowLink)}>
+            Escrowed - Tebex
+          </button>
+        )}
+        {props.opensourceLink && (
+          <button className="active" onClick={() => navigateTo(props.opensourceLink)}>
+            Open Source - Tebex
+          </button>
+        )}
 
-        <button className="active" onClick={() => navigateTo(props.escrowLink)}>
-          Escrowed - Tebex
-        </button>
+        {props.githubLink && <span>Download</span>}
+        
+        {props.githubLink && (
+          <button className="active" onClick={() => navigateTo(props.githubLink)}>
+            Github Link
+          </button>
+        )}
 
-        <button className="active" onClick={() => navigateTo(props.opensourceLink)}>
-          Open Source - Tebex
-        </button>
+        {props.comingSoon && (
+          <button className="deactivate" disabled>
+            Coming Soon
+          </button>
+        )}
       </div>
     </li>
   );
